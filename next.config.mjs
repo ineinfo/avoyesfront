e5d01b54248f62c296c75085162b7986a19acac4
ineinfotechservices <nikhil.ineinfotech@gmail.com>
2025-01-01 18:01:22 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ['example.com'], // Specify external image domains here if needed
+    },
+    webpack(config) {
+        // Add support for importing SVG files as React components
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        // Add support for importing CSS files
+        config.module.rules.push({
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        });
+
+        return config;
     },
 };
 
