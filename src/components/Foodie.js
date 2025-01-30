11c9bc7ee288614217ed5e618bbc9fea3e44c7e9
ineinfotechservices <nikhil.ineinfotech@gmail.com>
2025-01-30 14:43:32 +0530

    update foddie

--- src/components/Foodie.js
+++ src/components/Foodie.js
@@ -583,7 +583,7 @@ const Foodie = () => {
                                         alt="Right Banner"
                                         style={{ height: screens.sm ? 300 : 200 }}
                                     />
-                                    <div className="text" style={screens.sm ? {} : { marginTop: "-10px" }}>
+                                    <div className="text" style={screens.sm ? { marginTop: "-40px", marginLeft: "8px" } : { marginTop: "-10px" }}>
                                         <h3 style={{ fontSize: screens.sm ? "" : "20px" }}>{"Default Right Text"}</h3>
                                         <div className="view-more-btn" style={{ fontSize: screens.sm ? "" : "15px" }}>
                                             <Link
