3d9808b235060e2d5cae040889376bfcfe8d8858
thakkardharaine <dhara.ineinfotech@gmail.com>
2025-02-10 12:40:03 +0530

    food slider

--- src/components/Carousel.nikhil.js
+++ src/components/Carousel.nikhil.js
@@ -487,7 +487,7 @@ const CustomCarouselSix = () => {
           <Slider
             {...settings}
             className="pt-4 related-product"
-          // style={{ minWidth: "99vw", marginLeft: "-295px" }}
+          style={screens.sm ?{}:{ minWidth: "99vw", marginLeft: "-295px" }}
           >
             {foodItems.map((item) => (
               <div key={item.id} className="col-xl-3 market-slide item">
